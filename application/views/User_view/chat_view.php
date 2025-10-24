ca3d7269ca069b182a8d2d53420691751704c86b
Trong Dai <caotrongdai2004cd@gmail.com>
2025-10-24 21:32:15 +0700

    cập nhật lại nhắn tin user với admin

--- application/views/User_view/chat_view.php
+++ application/views/User_view/chat_view.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Chat Hỗ Trợ</title>
-<link rel="stylesheet" href="<?php echo base_url(); ?>public\css\chat_view.css?=1.2">
+<link rel="stylesheet" href="<?php echo base_url(); ?>public\css\chat_view.css?=1.7">
 
 </head>
 <body>
@@ -11,17 +11,116 @@
     <?php $this->load->view('components/header') ?> 
 
     <!-- Chat -->
-    <div class="main-content">
+    <div class="row">
+        <div class="col-8 backgrout ">
+          
+             <div class="book__category">
+            <div class="book__category-text">
+                  <span>SÁCH BÁN CHẠY</span>
+            </div>
+        </div>
+
+
+        <section>
+                <div class="container">
+    <div class="book">
+       
+         <div class="row">
+            <div class="col-3">
+                <div class="book__product">
+                    <a href="#">
+                    <div class="book__product-image">
+                        <img src="<?php echo base_url(); ?>\public\images\sp18s.jpg" alt="sản phẩm 1">
+                    </div>
+                    <div class="book__product-name">
+                        Truyền thuyết về mặt trời -tập 2 có 
+                    </div>
+                     <div class="book__product-price">
+                        80.000 VND 
+                    </div>
+                    </a>
+
+
+                </div>
+            </div>
+             <div class="col-3">
+                <div class="book__product">
+                    <a href="#">
+                    <div class="book__product-image">
+                        <img src="<?php echo base_url(); ?>\public\images\sp21t.jpg" alt="sản phẩm 1">
+                    </div>
+                    <div class="book__product-name">
+                        Tư tưởng Hồ Chí Minh
+                    </div>
+                     <div class="book__product-price">
+                        80.000 VND 
+                    </div>
+                    </a>
+
+
+                </div>
+            </div>
+             <div class="col-3">
+                <div class="book__product">
+                    <a href="#">
+                    <div class="book__product-image">
+                        <img src="<?php echo base_url(); ?>\public\images\sp27s.jpg" alt="sản phẩm 1">
+                    </div>
+                    <div class="book__product-name">
+                    anh hùng vũ trụ
+                    </div>
+                     <div class="book__product-price">
+                        80.000 VND 
+                    </div>
+                    </a>
+
+
+                </div>
+            </div> <div class="col-3">
+                <div class="book__product">
+                    <a href="#">
+                    <div class="book__product-image">
+                        <img src="<?php echo base_url(); ?>\public\images\sp20t.jpg" alt="sản phẩm 1">
+                    </div>
+                    <div class="book__product-name">
+                        Truyền thuyết về mặt trời -tập 2
+                    </div>
+                     <div class="book__product-price">
+                        80.000 VND 
+                    </div>
+                    </a>
+                </div>
+            </div>  
+        </div>
+    </div>
+    </div>
+</section>
+        </div>
+
+
+
+
+
+
+
+
+    <div class="col-4  ">
+        <div class="chat">
+            
+        
+    
         <div class="chat-container">
             <div class="chat-header">Hỗ trợ khách hàng</div>
             <div class="chat-box" id="chat-box"></div>
             <div class="chat-input">
                 <input type="text" id="message" placeholder="Nhập tin nhắn...">
-                <button id="sendBtn">Gửi</button>
+                <button id="sendBtn" class="sent">Gửi</button>
             </div>
+    
+    </div>
+</div>
         </div>
     </div>
-
     
     <?php $this->load->view('components/footer') ?>
 
